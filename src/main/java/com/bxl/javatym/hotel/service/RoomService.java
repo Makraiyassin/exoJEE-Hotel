55bedbd643929881558eb5cac3924b88311b3f48
Makraiyassin <makrai.yassin@gmail.com>
2022-07-15 14:17:09 +0200

    db + debut room service

--- src/main/java/com/bxl/javatym/hotel/service/RoomService.java
+++ src/main/java/com/bxl/javatym/hotel/service/RoomService.java
@@ -1,4 +1,17 @@
 package com.bxl.javatym.hotel.service;
 
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RoomService {
+    private static RoomService instance;
+    public static RoomService getInstance(){
+        System.out.println("recup de l'instance du service");
+        return instance == null ? instance = new RoomService() : instance;
+    }
+    private RoomService() {}
+
+    private final List<Room> roomList = new ArrayList<>();
+
 }
--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -16,8 +16,8 @@ CREATE TABLE Booking(
                         booking_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
                         customer_id int NOT NULL,
                         room_id int NOT NULL,
-                        first_name VARCHAR(255) NOT NULL,
-                        last_name VARCHAR(255) NOT NULL,
+                        date_begin DATE NOT NULL,
+                        date_end DATE NOT NULL,
                         CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-                        CONSTRAINT fk_room_id FOREIGN KEY (room_id) REFERENCES room(room_id),
+                        CONSTRAINT fk_room_id FOREIGN KEY (room_id) REFERENCES room(room_id)
 );
