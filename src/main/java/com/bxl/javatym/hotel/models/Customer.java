12623c70d156e7588c8334af35b63d3b2005d1a2
Therence <T@hotmail.com>
2022-07-15 14:27:04 +0200

    we

---
+++ src/main/java/com/bxl/javatym/hotel/models/Customer.java
@@ -0,0 +1,30 @@
+package com.bxl.javatym.hotel.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@NamedQueries({
+        @NamedQuery(name = "get_all", query = "SELECT c FROM Customer c"),
+        @NamedQuery(name = "remove_one", query = "DELETE FROM Customer c WHERE c.id = :id")
+})
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+}
