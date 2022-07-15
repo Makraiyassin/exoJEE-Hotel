bc7c36a9fc913c02a9ee00d8c603f27f80ab42c3
Yassin Makrai <makrai.yassin@gmail.com>
2022-07-15 18:38:57 +0200

    db insert some rooms

--- src/main/java/com/bxl/javatym/hotel/servlet/RoomsServlet.java
+++ src/main/java/com/bxl/javatym/hotel/servlet/RoomsServlet.java
@@ -14,13 +14,6 @@ public class RoomsServlet extends HttpServlet {
     private final RoomService roomService = RoomService.getInstance();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        int id = 0;
-        try{
-            id = Integer.parseInt(request.getParameter("id"));
-        }catch (Exception e){
-            System.out.println(e);
-        }
         List<Room> roomList = roomService.getAll();
         request.setAttribute("rooms", roomList);
         request.getRequestDispatcher("/hotel/index.jsp").forward(request,response);
